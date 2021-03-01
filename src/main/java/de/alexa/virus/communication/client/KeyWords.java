31dc670dbfc793ed097d4230a2cd4d7f22ab35a1
Rares <rsahleanu@gmail.com>
2021-03-01 11:43:10 +0100

    .

---
+++ src/main/java/de/alexa/virus/communication/client/KeyWords.java
@@ -0,0 +1,14 @@
+package de.alexa.virus.communication.client;
+
+public enum KeyWords {
+
+    CHANNEL_TRANSMISSION_END("channel:end");
+
+        String psn;
+     KeyWords(String str){
+
+        psn = str;
+
+    }
+
+}
